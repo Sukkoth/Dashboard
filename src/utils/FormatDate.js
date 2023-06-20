6000ca4893913c9bdd92e468750afd0c5a0707c0
Sukkoth <suukootj@gmail.com>
2023-06-20 14:11:02 +0300

    Format date function

---
+++ src/utils/FormatDate.js
@@ -0,0 +1,8 @@
+const FormatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { month: 'long', day: 'numeric', year: 'numeric' };
+    const formattedDate = date.toLocaleString('en-US', options);
+    return formattedDate;
+};
+
+export default FormatDate;
