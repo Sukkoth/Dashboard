8bfc48711c8b189e03e15f2cba6e4c0025f53cc8
Sukkoth <suukootj@gmail.com>
2023-10-12 14:23:26 +0300

    submit button div

--- src/Components/AddContract/Forms/SubmitButton.jsx
+++ src/Components/AddContract/Forms/SubmitButton.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const SubmitButton = ({ isLoading }) => {
     return (
-        <button className='btn btn-primary w-25 mx-auto mb-5' type='submit'>
-            {(isLoading && 'Loading . . .') || 'Add Contract'}
-        </button>
+        <div className='col-12  d-flex justify-content-center'>
+            <button className='btn btn-primary w-25 mb-5' type='submit'>
+                {(isLoading && 'Loading . . .') || 'Add Contract'}
+            </button>
+        </div>
     );
 };
 
