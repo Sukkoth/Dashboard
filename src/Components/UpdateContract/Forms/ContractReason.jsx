c6c52f52fbdeb9b798477e0ce18a66b150232548
Sukkoth <suukootj@gmail.com>
2024-01-30 10:37:45 +0300

    required on installement dates, no space on file names

--- src/Components/UpdateContract/Forms/ContractReason.jsx
+++ src/Components/UpdateContract/Forms/ContractReason.jsx
@@ -4,162 +4,168 @@ import useApiFetch from '../../../API/useApiFetch';
 import { UpdateContext } from '../../../Providers/UpdateProvider';
 
 const ContractReason = ({ register, errors, setValue }) => {
-    const { contractData } = useContext(UpdateContext);
-    const [changeFile, setFileChange] = useState(false);
-    if (!contractData) return;
-    return (
-        <div className='col-sm-12 col-xl-8'>
-            <div className='bg-white rounded h-100 p-4'>
-                <h6 className='mb-4 h4'>
-                    Contract Info
-                    <span className='text-danger'>*</span>
-                </h6>
-                <div>
-                    <div className='mb-3'>
-                        <label htmlFor='regionName mt-2'>Contract Type</label>
-                        <select
-                            id='region'
-                            name='region'
-                            className='form-select my-2 mb-3'
-                            defaultValue={''}
-                            {...register('contractType')}
-                        >
-                            <option value='' disabled>
-                                Select Contract Type
-                            </option>
-                            <option value='Building'>Building</option>
-                            <option value='Land'>Land</option>
-                            <option value='ATM'>ATM</option>
-                        </select>
-                        {errors?.region && (
-                            <div className='form-text text-danger mb-4'>
-                                {errors?.contractType?.message}
-                            </div>
-                        )}
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor='reason' className='mb-2'>
-                            Contract Type
-                        </label>
-                        <textarea
-                            className='form-control'
-                            placeholder='Leave contract reason here'
-                            id='reason'
-                            name='contractReason'
-                            style={{ height: '200px' }}
-                            {...register('contractReason')}
-                        ></textarea>
-                    </div>
-                    {errors?.contractReason && (
-                        <div className='form-text text-danger'>
-                            {errors?.contractReason?.message}
-                        </div>
-                    )}
-
-                    <div className='mb-3'>
-                        <label htmlFor='reason' className='mb-2'>
-                            Lease agreement File
-                            {!changeFile && (
-                                <span className='text-primary'>
-                                    📂 {contractData.fileName}
-                                </span>
-                            )}
-                            <span>
-                                <button
-                                    className='btn btn-outline-warning mx-3'
-                                    type='button'
-                                    onClick={() =>
-                                        setFileChange((prev) => !prev)
-                                    }
-                                >
-                                    {changeFile
-                                        ? 'Cancel changing file'
-                                        : 'Change file'}
-                                </button>
-                            </span>
-                        </label>
-                        {changeFile && (
-                            <AddFile
-                                register={register}
-                                error={errors?.fileName?.message}
-                                setValue={setValue}
-                            />
-                        )}
-                    </div>
-                </div>
+  const { contractData } = useContext(UpdateContext);
+  const [changeFile, setFileChange] = useState(false);
+  if (!contractData) return;
+  return (
+    <div className='col-sm-12 col-xl-8'>
+      <div className='bg-white rounded h-100 p-4'>
+        <h6 className='mb-4 h4'>
+          Contract Info
+          <span className='text-danger'>*</span>
+        </h6>
+        <div>
+          <div className='mb-3'>
+            <label htmlFor='regionName mt-2'>Contract Type</label>
+            <select
+              id='region'
+              name='region'
+              className='form-select my-2 mb-3'
+              defaultValue={''}
+              {...register('contractType')}
+            >
+              <option value='' disabled>
+                Select Contract Type
+              </option>
+              <option value='Building'>Building</option>
+              <option value='Land'>Land</option>
+              <option value='ATM'>ATM</option>
+            </select>
+            {errors?.region && (
+              <div className='form-text text-danger mb-4'>
+                {errors?.contractType?.message}
+              </div>
+            )}
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='reason' className='mb-2'>
+              Contract Type
+            </label>
+            <textarea
+              className='form-control'
+              placeholder='Leave contract reason here'
+              id='reason'
+              name='contractReason'
+              style={{ height: '200px' }}
+              {...register('contractReason')}
+            ></textarea>
+          </div>
+          {errors?.contractReason && (
+            <div className='form-text text-danger'>
+              {errors?.contractReason?.message}
             </div>
+          )}
+
+          <div className='mb-3'>
+            <label htmlFor='reason' className='mb-2'>
+              Lease agreement File
+              {!changeFile && (
+                <span className='text-primary'>📂 {contractData.fileName}</span>
+              )}
+              <span>
+                <button
+                  className='btn btn-outline-warning mx-3'
+                  type='button'
+                  onClick={() => setFileChange((prev) => !prev)}
+                >
+                  {changeFile ? 'Cancel changing file' : 'Change file'}
+                </button>
+              </span>
+            </label>
+            {changeFile && (
+              <AddFile
+                register={register}
+                error={errors?.fileName?.message}
+                setValue={setValue}
+              />
+            )}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 function AddFile({ register, error, setValue }) {
-    const [file, setFile] = useState('');
-    const {
-        data: fileData,
-        isLoading: fileUploading,
-        errors: fileUploadError,
-        fetchData: uploadFile,
-    } = useApiFetch(
-        {
-            url: '/leases/file',
-            method: 'POST',
-        },
-        false
-    );
+  const [file, setFile] = useState('');
+  const [localError, setLocalError] = useState('');
 
-    function handleSetFile(e) {
-        setFile((prev) => '');
-        if (e.target?.files[0]?.type === 'application/pdf') {
-            setFile((prev) => e.target.files[0]);
-        }
+  const {
+    data: fileData,
+    isLoading: fileUploading,
+    errors: fileUploadError,
+    fetchData: uploadFile,
+  } = useApiFetch(
+    {
+      url: '/leases/file',
+      method: 'POST',
+    },
+    false
+  );
+
+  function handleSetFile(e) {
+    setFile((prev) => '');
+    setLocalError(() => '');
+    if (e.target?.files[0]?.type === 'application/pdf') {
+      if (!e.target.files[0].name.includes(' ')) {
+        setFile((prev) => e.target.files[0]);
+      } else {
+        setLocalError('The file name should NOT include spaces');
+      }
+    } else {
+      setLocalError('Make sure the file is pdf');
     }
+  }
 
-    async function handleUpload() {
-        if (fileData?.fileName) return;
-        const formData = new FormData();
-        formData.append('file', file);
-        await uploadFile({
-            data: formData,
-        });
-    }
+  async function handleUpload() {
+    if (fileData?.fileName) return;
+    const formData = new FormData();
+    formData.append('file', file);
+    await uploadFile({
+      data: formData,
+    });
+  }
 
-    useEffect(() => {
-        setValue('fileName', fileData?.fileName);
-    }, [fileData?.fileName]);
-    return (
-        <div>
-            {!fileData?.fileName && (
-                <input
-                    className='mx-2 border-primary'
-                    type='file'
-                    accept='application/pdf'
-                    name='leaseContract'
-                    id='leaseContract'
-                    onChange={handleSetFile}
-                />
-            )}
-            <button
-                className={`btn ${
-                    fileData?.fileName ? 'btn-primary' : 'btn-outline-primary'
-                }`}
-                type='button'
-                disabled={fileUploading || !file}
-                onClick={handleUpload}
-            >
-                {fileData?.fileName
-                    ? 'File uploaded'
-                    : !fileUploading
-                    ? 'Submit File'
-                    : 'Uploading'}
+  useEffect(() => {
+    setValue('fileName', fileData?.fileName);
+  }, [fileData?.fileName, setValue]);
+  return (
+    <div>
+      {!fileData?.fileName && (
+        <input
+          className='mx-2 border-primary'
+          type='file'
+          accept='application/pdf'
+          name='leaseContract'
+          id='leaseContract'
+          onChange={handleSetFile}
+        />
+      )}
+      <button
+        className={`btn ${
+          fileData?.fileName || file
+            ? 'btn-primary'
+            : 'btn-outline-danger cursor-not-allowed'
+        }`}
+        type='button'
+        disabled={fileUploading || !file}
+        onClick={handleUpload}
+      >
+        {fileData?.fileName
+          ? 'File uploaded'
+          : !fileUploading
+          ? 'Submit File'
+          : 'Uploading'}
 
-                {fileUploading && <CircleLoader size={5} color='white' />}
-            </button>
-            {fileData?.fileName && (
-                <p className='d-inline mx-3 text-primary'>{file?.name}</p>
-            )}
-            {error && <div className='form-text text-danger'>{error}</div>}
-        </div>
-    );
+        {fileUploading && <CircleLoader size={5} color='white' />}
+      </button>
+      {fileData?.fileName && (
+        <p className='d-inline mx-3 text-primary'>{file?.name}</p>
+      )}
+      {error && <div className='form-text text-danger'>{error}</div>}
+      {localError && <div className='form-text text-danger'>{localError}</div>}
+    </div>
+  );
 }
 
 export default ContractReason;
--- src/Components/UpdateContract/Forms/InstallmentData.jsx
+++ src/Components/UpdateContract/Forms/InstallmentData.jsx
@@ -1,136 +1,124 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { UpdateContext } from '../../../Providers/UpdateProvider';
 const InstallmentData = ({ installmentData, setInstallmentData }) => {
-    const { contractData } = useContext(UpdateContext);
-    const [usedData, setUsedData] = useState(false);
+  const { contractData } = useContext(UpdateContext);
+  const [usedData, setUsedData] = useState(false);
 
-    let installmentDetails = null;
-    if (contractData?.installmentDetails) {
-        installmentDetails = JSON.parse(contractData?.installmentDetails);
-    }
+  let installmentDetails = null;
+  if (contractData?.installmentDetails) {
+    installmentDetails = JSON.parse(contractData?.installmentDetails);
+  }
 
-    function handleAddInstallmentData() {
-        setInstallmentData((prev) => [
-            ...prev,
-            { installmentDate: '', amount: 0 },
-        ]);
-    }
+  function handleAddInstallmentData() {
+    setInstallmentData((prev) => [...prev, { installmentDate: '', amount: 0 }]);
+  }
 
-    function removeInstallmentData(index) {
-        setInstallmentData((prev) => prev.filter((data, i) => i !== index));
-    }
-    function handleInstallmentDateChange(installmentIndex, date) {
-        setInstallmentData((prev) =>
-            prev.map((data, index) =>
-                index === installmentIndex
-                    ? { ...data, installmentDate: date }
-                    : data
-            )
-        );
-    }
-
-    function handleInstallmentAmountChange(installmentIndex, amount) {
-        setInstallmentData((prev) =>
-            prev.map((data, index) =>
-                index === installmentIndex ? { ...data, amount: amount } : data
-            )
-        );
-    }
-
-    useEffect(() => {
-        if (
-            !usedData &&
-            installmentDetails !== null &&
-            installmentData?.length === 0
-        ) {
-            setInstallmentData(
-                Object.entries(installmentDetails).map(([date, value]) => ({
-                    installmentDate: date,
-                    amount: value,
-                }))
-            );
-            setUsedData(true);
-        }
-    }, [installmentDetails, usedData]);
-
-    return (
-        <div className='col-sm-12 col-xl-6'>
-            <div className='bg-white rounded h-100 p-4'>
-                <h6 className='mb-4 h4'>
-                    Installment Data{' '}
-                    <button
-                        className='btn btn-primary mx-3'
-                        type='button'
-                        onClick={handleAddInstallmentData}
-                    >
-                        +
-                    </button>
-                </h6>
-                <div>
-                    {installmentData?.map((data, index) => (
-                        <div className='row mb-2' key={index}>
-                            <div className='mb-3 col-6'>
-                                <label
-                                    htmlFor={`installmentDate${index}`}
-                                    className='form-label'
-                                >
-                                    Installment Date
-                                    <span className='text-danger'>*</span>
-                                </label>
-                                <input
-                                    type='date'
-                                    className='form-control'
-                                    id={`installmentDate${index}`}
-                                    min={0.0}
-                                    value={
-                                        installmentData[index]?.installmentDate
-                                    }
-                                    onChange={(e) =>
-                                        handleInstallmentDateChange(
-                                            index,
-                                            e.target.value
-                                        )
-                                    }
-                                />
-                            </div>
-                            <div className='mb-3 col-4'>
-                                <label
-                                    htmlFor={`installmentAmount${index}`}
-                                    className='form-label'
-                                >
-                                    Amount
-                                    <span className='text-danger'>*</span>
-                                </label>
-                                <input
-                                    type='number'
-                                    step={0.00005}
-                                    className='form-control'
-                                    id={`installmentAmount${index}`}
-                                    min={0.0}
-                                    value={installmentData[index]?.amount}
-                                    onChange={(e) =>
-                                        handleInstallmentAmountChange(
-                                            index,
-                                            e.target.value
-                                        )
-                                    }
-                                />
-                            </div>
-                            <div className='mb-3 col-2 d-flex justify-content-center align-items-center mt-4'>
-                                <button
-                                    className='btn btn-danger'
-                                    type='button'
-                                    onClick={() => removeInstallmentData(index)}
-                                >
-                                    x
-                                </button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
+  function removeInstallmentData(index) {
+    setInstallmentData((prev) => prev.filter((data, i) => i !== index));
+  }
+  function handleInstallmentDateChange(installmentIndex, date) {
+    setInstallmentData((prev) =>
+      prev.map((data, index) =>
+        index === installmentIndex ? { ...data, installmentDate: date } : data
+      )
     );
+  }
+
+  function handleInstallmentAmountChange(installmentIndex, amount) {
+    setInstallmentData((prev) =>
+      prev.map((data, index) =>
+        index === installmentIndex ? { ...data, amount: amount } : data
+      )
+    );
+  }
+
+  useEffect(() => {
+    if (
+      !usedData &&
+      installmentDetails !== null &&
+      installmentData?.length === 0
+    ) {
+      setInstallmentData(
+        Object.entries(installmentDetails).map(([date, value]) => ({
+          installmentDate: date,
+          amount: value,
+        }))
+      );
+      setUsedData(true);
+    }
+  }, [installmentDetails, usedData]);
+
+  return (
+    <div className='col-sm-12 col-xl-6'>
+      <div className='bg-white rounded h-100 p-4'>
+        <h6 className='mb-4 h4'>
+          Installment Data{' '}
+          <button
+            className='btn btn-primary mx-3'
+            type='button'
+            onClick={handleAddInstallmentData}
+          >
+            +
+          </button>
+        </h6>
+        <div>
+          {installmentData?.map((data, index) => (
+            <div className='row mb-2' key={index}>
+              <div className='mb-3 col-6'>
+                <label
+                  htmlFor={`installmentDate${index}`}
+                  className='form-label'
+                >
+                  Installment Date
+                  <span className='text-danger'>*</span>
+                </label>
+                <input
+                  required
+                  type='date'
+                  className='form-control'
+                  id={`installmentDate${index}`}
+                  min={0.0}
+                  value={installmentData[index]?.installmentDate}
+                  onChange={(e) =>
+                    handleInstallmentDateChange(index, e.target.value)
+                  }
+                />
+              </div>
+              <div className='mb-3 col-4'>
+                <label
+                  htmlFor={`installmentAmount${index}`}
+                  className='form-label'
+                >
+                  Amount
+                  <span className='text-danger'>*</span>
+                </label>
+                <input
+                  type='number'
+                  step={0.00005}
+                  className='form-control'
+                  id={`installmentAmount${index}`}
+                  min={0.0}
+                  value={installmentData[index]?.amount}
+                  onChange={(e) =>
+                    handleInstallmentAmountChange(index, e.target.value)
+                  }
+                />
+              </div>
+              <div className='mb-3 col-2 d-flex justify-content-center align-items-center mt-4'>
+                <button
+                  className='btn btn-danger'
+                  type='button'
+                  onClick={() => removeInstallmentData(index)}
+                >
+                  x
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InstallmentData;
